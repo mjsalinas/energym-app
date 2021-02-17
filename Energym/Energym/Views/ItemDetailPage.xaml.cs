1b3cb85d1816f4455d3a07fb2df4a5a96036009a
Aaron Lemus <aalberto2695@gmail.com>
2021-02-16 21:49:51 -0600

    se agrego la el proyectyo de xamarin forms.

---
+++ Energym/Energym/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Energym.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Energym.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
