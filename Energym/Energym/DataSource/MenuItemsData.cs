dcb32f5a1f85570890fe05c7c02efe92ec8b0940
Maria Salinas <mariasalinas713@gmail.com>
2021-02-23 21:47:42 -0600

    -Agregados iconos para miniaturas - Prueba de navbar menu y tab menu para la aplicacion, falta implementar con pantallas existentes

---
+++ Energym/Energym/DataSource/MenuItemsData.cs
@@ -0,0 +1,24 @@
+﻿using Energym.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energym.DataSource
+{
+    public class MenuItemsData
+    {
+        public List<MenuItems> MenuItems = new List<MenuItems>()
+        {
+            new MenuItems()
+            {
+                Modulo = "Clientes",
+                Pantallas = {"Registrar Nuevo Cliente","Modificar Cliente Existente","Registrar Pago"}
+            },
+            new MenuItems()
+            {
+                Modulo = "Configuraciones",
+                Pantallas = {"Grupos","Tipo de Planes","Unidades Medida","Oportunidades"}
+            }
+        };
+    }
+}
---
+++ Energym/Energym/Models/MenuItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Energym.Models
+{
+    public class MenuItems
+    {
+        public string Modulo { get; set; }
+
+        public List<string> Pantallas { get; set; }
+    }
+}
