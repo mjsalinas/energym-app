fc8f0a9ceaac5417d672c67add6d6a74049bb90d
Maria Salinas <mariasalinas713@gmail.com>
2021-03-05 01:29:37 -0600

    -Agregado el modelo para Clientes -Instalada libreria para JSON -Conexion a servicios GET y POST para Registro de Clientes

--- Energym/Energym/ViewModels/ClientesViewModel/RegistrarClienteViewModel.cs
+++ Energym/Energym/ViewModels/ClientesViewModel/RegistrarClienteViewModel.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Energym.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -14,11 +17,13 @@ namespace Energym.ViewModels.ClientesViewModel
         {
             RegistrarClienteCommand = new Command(async () => await RegistrarCliente());
             CancelarCommand = new Command(CancelarRegistroCliente);
+            CargarClientes();
         }
+        public List<Cliente> Clientes { get; set; }
         public Command RegistrarClienteCommand { get; } 
         public Command CancelarCommand { get; }
-
         public event PropertyChangedEventHandler PropertyChanged;
+
 
         string nombre = "maria";
         string numeroTelefono = string.Empty;
@@ -27,7 +32,7 @@ namespace Energym.ViewModels.ClientesViewModel
         int tipoPlan;
         sbyte activo = 1;
         string estadoCliente = string.Empty;
-
+        int idGrupo;
         public string Nombre
         {
             get { return nombre; }
@@ -68,13 +73,39 @@ namespace Energym.ViewModels.ClientesViewModel
         async Task RegistrarCliente ()
         {
             // asignacion de campos y data a mandar a servicios
+            Cliente nuevoCliente = new Cliente()
+            {
+                Nombre = nombre,
+                NumeroTelefono = numeroTelefono,
+                Correo = correo,
+                Activo = activo,
+                EstadoCliente = estadoCliente,
+                TipoPlan = tipoPlan,
+                IdGrupo = idGrupo
+            };
             //llamada a servicios
-            await Task.Delay(400);
+            var json = JsonConvert.SerializeObject(nuevoCliente);
+            var registroNuevo = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpClient client = new HttpClient();
+        
+            var response = await client.PostAsync("http://157.230.13.243/cliente", registroNuevo);
         }
         void CancelarRegistroCliente()
         {
             Nombre = string.Empty;
             NumeroTelefono = string.Empty;
+        }
+        async Task CargarClientes()
+        {
+            HttpClient client = new HttpClient();
+
+            var response = await client.GetAsync("http://157.230.13.243/cliente");
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                string objetoRespuesta = await response.Content.ReadAsStringAsync();
+                Clientes = JsonConvert.DeserializeObject<IEnumerable<Cliente>>(objetoRespuesta) as List<Cliente>;
+            }
+            //return response.
         }
     }
 }
